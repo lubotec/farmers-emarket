c84896ba5f99d341c088bd1f8b9f7f505c933b51
Joel Velis <joelvelis94@gmail.com>
2019-05-29 21:00:18 +0200

    add create and destroy method to ProductPhotosController

--- app/controllers/product_photos_controller.rb
+++ app/controllers/product_photos_controller.rb
@@ -1,2 +1,30 @@
 class ProductPhotosController < ApplicationController
+    before_action :set_product, only: [:index, :create]
+    
+    # def index
+    #     @product_photo = ProductPhoto.new
+    #     @product_photos = ProductPhoto.where(product: @product)
+    # end
+
+    def create
+        @product_photo = ProductPhoto.new(product_photo_params)
+        @product_photo.product = @product
+        @product_photo.save
+        redirect_to farmer_products_path
+    end
+
+    def destroy
+        @product_photo.destroy
+        redirect_to farmer_products_path
+    end
+
+    private
+
+    def product_photo_params
+        params.require(:product_photo).permit(:data)
+    end
+
+    def set_product
+        @product = Product.find(params[:product_id])
+    end
 end
