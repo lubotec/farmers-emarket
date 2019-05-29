6e524ef32e231fd7d6dfb49ba259029033ea14f5
Joel Velis <joelvelis94@gmail.com>
2019-05-29 15:51:57 +0200

    create view pages for products

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :products
   resources :restaurants, only: [ :new, :create ]
   resources :farmers, only: [:new, :create, :show ]
-  get '/farmers/:id/products', to: 'products#my_products', as: 'farmer_products'
+  get 'my_products', to: 'products#my_products', as: 'farmer_products'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
