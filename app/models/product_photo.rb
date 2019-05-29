d3cd5e123d80d3fc04654b82f430e578bd80ef24
Joel Velis <joelvelis94@gmail.com>
2019-05-29 22:54:06 +0200

    add method to productphotos controller

--- app/models/product_photo.rb
+++ app/models/product_photo.rb
@@ -1,3 +1,28 @@
 class ProductPhoto < ApplicationRecord
   belongs_to :product
+  before_action :set_product, only: [:index, :create]
+    
+# def index
+#     @product_photo = ProductPhoto.new
+#     @product_photos = ProductPhoto.where(product: @product)
+# end
+
+def create
+    @product_photo = ProductPhoto.new(product_photo_params)
+    @product_photo.product = @product
+    @product_photo.save
+    # redirect_to
 end
+
+def destroy
+end
+
+private
+
+def product_photo_params
+    params.require(:product_photo).permit(:data)
+end
+
+def set_product
+    @product = Product.find(params[:product_id])
+endd
