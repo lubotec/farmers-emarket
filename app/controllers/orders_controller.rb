4224278eef79a9fb168b0f0479c05209e212f79d
Lukas Bollinger <lukas.bollinger21@gmail.com>
2019-05-30 13:13:07 +0200

    order checkout logic and order views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,32 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :checkout]
+
   def show
-    @order = Order.find(params[:id])
+  end
+
+  def my_active_order
+    @order = current_user.restaurant.active_order
+  end
+
+  def checkout_order
+    if @order.update(status: "paid")
+      @order.order_products.each do |order_product|
+        order_product.update(status: "paid")
+      end
+      redirect_to restaurant_orders_path(current_user.restaurant)
+    else
+      render 'my_active_order'
+      flash[:error] = "Unfortunately, something went wrong. Try again!"
+    end
   end
 
   def my_orders
     @orders = current_user.restaurant.orders
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
