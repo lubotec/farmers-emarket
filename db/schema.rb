19efe39b1f71c429b661aab30476e01c3ec4512b
Philip Chan <philip.chan92@icloud.com>
2019-06-03 17:37:47 +0200

    add geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_144733) do
+ActiveRecord::Schema.define(version: 2019_06_03_152758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_05_30_144733) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_farmers_on_user_id"
   end
 
