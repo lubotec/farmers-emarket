018267be9f1ac6b6fc31466ab63b469e38e50277
Philip Chan <philip.chan92@icloud.com>
2019-06-03 19:10:51 +0200

    add geocoding to farmers address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_152758) do
+ActiveRecord::Schema.define(version: 2019_06_03_155734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,6 @@ ActiveRecord::Schema.define(version: 2019_06_03_152758) do
     t.bigint "user_id"
     t.string "name"
     t.string "address"
-    t.float "lat"
-    t.float "long"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
