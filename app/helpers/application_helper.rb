018b7e94cc37032ba180a6996542e951c499ab53
Lukas Bollinger <lukas.bollinger21@gmail.com>
2019-06-04 12:37:41 +0200

    Category filter independent from text search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
   def query_hash(query, key_to_update, new_value)
     query = {} if query.blank?
+    return if query.key?(key_to_update)
     query[key_to_update] = new_value
     query
+  end
+
+  def selected_category?(category)
+    params[:query].try(:[], :category) == category
   end
 end
