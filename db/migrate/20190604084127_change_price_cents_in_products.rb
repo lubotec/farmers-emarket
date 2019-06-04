0cbcf10b3d535a98da065c6699349d168054c860
Lukas Bollinger <lukas.bollinger21@gmail.com>
2019-06-04 19:16:33 +0200

    Delete two files which cuased error

--- db/migrate/20190604084127_change_price_cents_in_products.rb
+++
@@ -1,4 +0,0 @@
-class ChangePriceCentsInProducts < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20190604085605_remove_price_cents_from_products.rb
+++
@@ -1,5 +0,0 @@
-class RemovePriceCentsFromProducts < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :products, :price_cents, :integer
-  end
-end
