1ac9037b6237319dbde499c06c273541f440f7d2
Jackson Huang <jacksonh201325@gmail.com>
2019-06-05 14:16:54 +0200

    fixed simple form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -64,6 +64,15 @@ SimpleForm.setup do |config|
     # b.use :full_error, wrap_with: { tag: :span, class: :error }
   end
 
+  config.wrappers do |b|
+    b.use :placeholder
+    b.use :label_input
+    b.wrapper tag: :div, class: 'separator' do |component|
+      component.use :hint,  wrap_with: { tag: :span, class: :hint }
+      component.use :error, wrap_with: { tag: :span, class: :error }
+    end
+  end
+
   # The default wrapper to be used by the FormBuilder.
   config.default_wrapper = :default
 
